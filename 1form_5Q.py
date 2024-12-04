e8982f6f041cac1addab004db87eb38f449888a8
Papipappoko <kewza9616@gmail.com>
2024-12-04 16:05:57 +0700

    Add files via upload

---
+++ 1form_5Q.py
@@ -0,0 +1,12 @@
+print("sawat dee kub")
+nickname = input("กรอกชื่อเล่นให้หน่อย eiei : ")
+weight = int(input("น้ำหนัก : "))
+height = int(input("ส่วนสูง : \n"))
+
+print("="*100 + "\n")
+      
+print("หุ่นแกดีมากอ่ะแกร\n")
+
+print("welcome nakub\n")
+
+print("วันนี้มีแค่นี้วันหลังมาใหม่นาาาาา")
